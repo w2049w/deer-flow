b9e7d6643701e7d6a963f8f6ba387496621bae2b
w2049w <826455283@qq.com>
2026-03-10 22:10:00 +0800

    正式注册了 news-reporter 和 news-editor 作为系统内置子智能体

--- backend/src/subagents/builtins/__init__.py
+++ backend/src/subagents/builtins/__init__.py
@@ -2,14 +2,20 @@
 
 from .bash_agent import BASH_AGENT_CONFIG
 from .general_purpose import GENERAL_PURPOSE_CONFIG
+from .news_reporter import NEWS_REPORTER_CONFIG
+from .news_editor import NEWS_EDITOR_CONFIG
 
 __all__ = [
     "GENERAL_PURPOSE_CONFIG",
     "BASH_AGENT_CONFIG",
+    "NEWS_REPORTER_CONFIG",
+    "NEWS_EDITOR_CONFIG",
 ]
 
 # Registry of built-in subagents
 BUILTIN_SUBAGENTS = {
     "general-purpose": GENERAL_PURPOSE_CONFIG,
     "bash": BASH_AGENT_CONFIG,
+    "news-reporter": NEWS_REPORTER_CONFIG,
+    "news-editor": NEWS_EDITOR_CONFIG,
 }
